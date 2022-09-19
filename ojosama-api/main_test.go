7787833747ccce0fd589cc6f9d2141361c37ba69
jiro4989 <jiroron666@gmail.com>
2022-09-19 18:11:13 +0900

    ファイルを移動してきた

---
+++ ojosama-api/main_test.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"testing"
+
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestHandler(t *testing.T) {
+	request := events.APIGatewayProxyRequest{
+		Body: `{"Text":"ハーブです"}`,
+	}
+	expectedResponse := events.APIGatewayProxyResponse{
+		StatusCode: 200,
+		Headers: map[string]string{
+			"Content-Type":                "application/json",
+			"Access-Control-Allow-Origin": "*",
+		},
+		Body: `{"Result":"おハーブですわ"}`,
+	}
+
+	response, err := Handler(request)
+
+	assert.Equal(t, response.Headers, expectedResponse.Headers)
+	assert.Contains(t, response.Body, expectedResponse.Body)
+	assert.Equal(t, err, nil)
+}
